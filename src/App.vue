c0923250dc5a0351e96f3a521b9a5dd4c8326bab
isaacwrk <isaacsouzawrk@gmail.com>
2021-10-21 18:57:41 -0300

    fixing api

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,39 @@
 <template>
 <div class="container grid-lg">
-  <h1>oi</h1>
+  <h1>{{quotes}}</h1>
 </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
+import { defineComponent, onMounted, reactive, toRefs } from "@vue/runtime-core"
+import api from '@/services/api'
 
+interface NewDataState{
+  quotes:any
+}
 
-@Options({
-  components: {
+const App = defineComponent({
+  components:{},
+  name:'App',
+  setup(){
+    const data = reactive<NewDataState>({
+      quotes:{}
+    })
 
-  },
+    onMounted(async() => {
+      const response = await api.all()
+      data.quotes = response.data
+    })
+    
+    return {...toRefs(data)}
+  }
 })
-export default class App extends Vue {}
+
+
+export default App;
 </script>
 
 <style>
 
 </style>
+
