4145e3d7a037e4c4faa02020d085abf404244b7b
isaacwrk <isaacsouzawrk@gmail.com>
2021-10-21 22:31:35 -0300

    creating listen api methods

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
 <div class="container grid-lg my-2 py-2">
+  <div class="card mb-2">
+    <div class="card-header">
+      <div class="h4">Acompanhando</div>
+    </div>
+    <div class="card-body">
+      <WatchListQuotes />
+    </div>
+  </div>
   <div class="card">
     <div class="card-header">
       <div class="h4">Todas as moedas</div>
@@ -15,13 +23,15 @@
 import { defineComponent, onMounted, reactive, toRefs } from "@vue/runtime-core"
 import api from '@/services/api'
 import ListQuotes from '@/components/ListQuotes.vue'
+import WatchListQuotes from '@/components/WatchListQuotes.vue'
+
 
 interface NewDataState{
   quotes:any
 }
 
 const App = defineComponent({
-  components:{ListQuotes},
+  components:{ListQuotes, WatchListQuotes},
   name:'App',
   setup(){
     const data = reactive<NewDataState>({
--- src/components/WatchListQuotes.vue
+++ src/components/WatchListQuotes.vue
@@ -1,16 +1,22 @@
 <template>
 <ListQuotes quotes="quotes"/>
+<div class="mt-2 text-right">
+<cite class="text-small">
+    Atualizará novamente em <b>{{nextUpdateTime}} segundos</b>
+</cite>
+</div>
 </template>
 
 <script lang='ts'>
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 import ListQuotes from './ListQuotes.vue'
 
 const listQuotes = defineComponent({
+    components:{ListQuotes},
     setup(){
         const quotes = ref({})
-
-        return{quotes}
+        const nextUpdateTime = ref(30)
+        return{quotes, nextUpdateTime}
     }
 });
 
