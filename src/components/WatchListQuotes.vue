c5d52bf63fc52a7be4399f9ac9b29e3852a625db
isaacwrk <isaacsouzawrk@gmail.com>
2021-10-21 22:21:52 -0300

    creating component to watch quotes

---
+++ src/components/WatchListQuotes.vue
@@ -0,0 +1,22 @@
+<template>
+<ListQuotes quotes="quotes"/>
+</template>
+
+<script lang='ts'>
+import { defineComponent } from 'vue';
+import ListQuotes from './ListQuotes.vue'
+
+const listQuotes = defineComponent({
+    setup(){
+        const quotes = ref({})
+
+        return{quotes}
+    }
+});
+
+export default listQuotes;
+</script>
+
+<style>
+
+</style>
